f64e9dc53775b30e757a64fbe6c8d07364cc4e08
TellSamm <chernikov.semen21@gmail.com>
2023-04-21 22:43:47 +0300

    -- new changes today

--- src/test/java/ReqresTest.java
+++ src/test/java/ReqresTest.java
@@ -1,23 +1,22 @@
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.get;
+
 
 
 public class ReqresTest {
 
+
+
+
     @Test
-    void singleUserCheckSchemeTest(){
-        given()
-                .log().uri()
-                .when()
-                .get("https://reqres.in/api/users/2")
+    void singleUserCheckSchemeTest() {
+        get("https://reqres.in/api/users/2")
                 .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("schemes/singleUserResponceScheme.json"));
+                .body("email","")
+
+
+
 
     }
+
+}
