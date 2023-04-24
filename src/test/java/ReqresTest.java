2073d343e289a7609f53c1f4540c9f1c58518f65
TellSamm <chernikov.semen21@gmail.com>
2023-04-24 23:27:29 +0300

    -- new changes delete deprecate code

--- src/test/java/ReqresTest.java
+++ src/test/java/ReqresTest.java
@@ -1,21 +1,22 @@
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasKey;
 
 
 public class ReqresTest {
 
+    public static final String BASE_URL = "https://reqres.in";
+
     @Test
-    void singleUserCheckSupportLinkTest() {
-        given() // предусловия configuration
+    void CheckSupportLinkTest() {
+        given()
                 .log().uri()
                 .when() // действие
-                .get("https://reqres.in/api/users/2")
+                .get(BASE_URL + "/api/users/2")
 
-                .then() // проверка
+                .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
@@ -25,4 +26,37 @@ public class ReqresTest {
     }
 
 
+    @Test
+    void CheckListUsersTest() {
+        given()
+                .log().uri()
+                .when()
+                .get(BASE_URL + "/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12))
+                .body(matchesJsonSchemaInClasspath("shemes/status-scheme-response-list-users.json"));
+
+    }
+
+
+    @Test
+    void holl() {
+//        String expectedResponse =
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .assertThat()
+                .body("support.url", equalTo("https://reqres.in/#support-heading"));
+
+
+    }
 }
