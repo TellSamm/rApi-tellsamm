7b468d0c6b7b12e261fde039868c94d35fa40412
TellSamm <chernikov.semen21@gmail.com>
2023-04-24 22:31:06 +0300

    -- new changes today

--- src/test/java/ReqresTest.java
+++ src/test/java/ReqresTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
+import static org.hamcrest.Matchers.hasKey;
 
 
 public class ReqresTest {
@@ -22,13 +22,33 @@ public class ReqresTest {
     @Test
     void singleUserCheckScheme2Test() {
         given() // предусловия configuration
-
+                .log().uri()
            .when() // действие
                 .get("https://reqres.in/api/users/2")
 
-           .then(). // проверка
-                assertThat().
-                body("data.email", equalTo("janet.weaver@reqres.in"));
+           .then() // проверка
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .assertThat()
+                .body("data.email", equalTo("janet.weaver@reqres.in"));
+
+    }
+
+
+    @Test
+    void singleUserCheckSupportLinkTest() {
+        given() // предусловия configuration
+                .log().uri()
+                .when() // действие
+                .get("https://reqres.in/api/users/2")
+
+                .then() // проверка
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .assertThat()
+                .body("support.url", equalTo("https://reqres.in/#support-heading"));
 
     }
 
