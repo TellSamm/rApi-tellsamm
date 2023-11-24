3ce19ce853ee6e64a7555f4c448bfa2182748337
TellSamm <chernikov.semen21@gmail.om>
2023-11-24 13:06:37 +0300

    test manual - 2

--- src/test/java/studies/ExampleGenerics.java
+++ src/test/java/studies/ExampleGenerics.java
@@ -14,19 +14,19 @@ public class ExampleGenerics<T, V> {
         if (this.string.equals("")) {
             this.string = this.string + t.toString() + v.toString();
         } else {
-            this.string = this.string + " - " + t.toString() + v.toString(); //test = 4
+            this.string = this.string + " - " + t.toString() + v.toString(); //test = 42
         }
     }
 
     public void addToStringTest(T tellSamm, V meTest) {
         String testSamm = this.string = this.string + " - " + tellSamm.toString() + meTest.toString();
-        System.out.println(testSamm); // test = 2
+        System.out.println(testSamm); // test = 22
     }
 
 
     public <J, P> void addToStringTest2(J tellSamm2, P meTest2) {
         String testSamm = this.string + " - " + tellSamm2.toString() + meTest2.toString();
-        System.out.println(testSamm);// test
+        System.out.println(testSamm);// test = false
     }
 
 }
